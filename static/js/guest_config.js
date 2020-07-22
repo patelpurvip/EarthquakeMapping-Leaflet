8f7e577586537694836cd74bc77115b4d112bc00
Purvi P. Patel <patel.purvi.p@gmail.com>
2020-07-22 01:33:18 -0500

    Update guest_config.js

--- static/js/guest_config.js
+++ static/js/guest_config.js
@@ -1,1 +1,3 @@
-const API_KEY = "YOUR API KEY HERE"
+const API_KEY = "pk.eyJ1IjoicGF0ZWxwdXJ2aXAiLCJhIjoiY2tjd3puaHZkMGR1ODMzbDU2eGs1NWEzNiJ9.iIWeRH8NFF9uN9IUOm4XQw"
+# REPLACE THE API KEY INSIDE THE QUOTES WITH YOUR KEY
+# This key will not work outside of this repository
